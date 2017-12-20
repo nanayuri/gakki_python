bd165450973070d934c39968faf1f51cb1dce07f
gakki <nanayuri_sunflow@git.com>
2017-12-20 18:00:41 +0800

    123

---
+++ cha4_test1.py
@@ -0,0 +1,5 @@
+useript = int(input('请输入一个整数：'))
+i = 1
+while useript >= i:
+    print(i)
+    i += 1
---
+++ cha4_test2.py
@@ -0,0 +1,4 @@
+useript = int(input('请输入一个整数：'))
+while useript > 0:
+    print(' '*useript) + print('*'*useript)
+    useript -= 1
